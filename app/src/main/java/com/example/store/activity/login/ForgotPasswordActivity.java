da37177be29b28ab1d80fc297f1205e7d9abb508
Ngọc <tojimomirudo54@gmail.com>
2023-04-11 23:54:10 +0700

    sửa chút giao diện

--- app/src/main/java/com/example/store/activity/login/ForgotPasswordActivity.java
+++ app/src/main/java/com/example/store/activity/login/ForgotPasswordActivity.java
@@ -4,12 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.store.R;
 
+
 public class ForgotPasswordActivity extends AppCompatActivity {
 
-    Button btn_back;
+    Button btn_back, btn_submit;
+
+    EditText reset_email;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +30,27 @@ public class ForgotPasswordActivity extends AppCompatActivity {
                 /*Intent intentBackLogin = new Intent(getBaseContext(), MainActivity.class);
                 startActivity(intentBackLogin);*/
                 finish();
+            }
+        });
+
+        //
+        reset_email = findViewById(R.id.reset_mail);
+        btn_submit = (Button) findViewById(R.id.btn_submit);
+        btn_submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth auth = FirebaseAuth.getInstance();
+                String emailAddress = "user@example.com";
+
+                auth.sendPasswordResetEmail(emailAddress)
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if (task.isSuccessful()) {
+                                    Log.d(TAG, "Email sent.");
+                                }
+                            }
+                        });
             }
         });
     }
